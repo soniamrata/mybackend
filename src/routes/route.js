259c86d70d0469b79e2a6dab71d00426fe0d8e29
amrata <soniamrata55@gmail.com>
2022-10-18 00:42:45 +0530

    get api

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,10 +10,10 @@ router.get('/test-me', function (req, res) {
 
 router.get('/students', function (req, res){
     console.log("The path params in the request are : ", req.params)
-    let students = ['Sabiha', 'Neha', 'Akash']
+    let students = ['Sabiha', 'Neha', 'sabiha']
     res.send(students)
 })
-
+//*-------------------------------
 
 // Example 1 for path params
 router.get('/students/:studentName', function(req, res){
@@ -41,3 +41,66 @@ router.get('/student-details/:name', function(req, res){
 })
 
 module.exports = router;
+
+//*----------------------------------------------ques 1--------------------------------------------------*//
+// router.get('/movies', function (req, res){
+    
+//     let movies = ['gadar', 'ek villan', 'genius','comando2']
+//     res.send(movies)
+// })
+
+
+//*-----------------------------------------------ques 2------------------------------------------------------*//
+// router.get('/movies/:indexNumber', function (req, res){
+// let movies = ['gadar', 'ek villan', 'genius','commando2','suryavanshi']
+// res.send(movies[req.params.indexNumber])
+// })
+
+
+//*------------------------------------------ques 3--------------------------------------------------------*//
+
+
+// router.get('/movies/:indexNumber', function (req, res) {
+//     let mymovies = ['Chichore', 'Genius', 'Ms.Dhoni', "3 Idiots", "Heropanti"]
+//     if (mymovies[req.params.indexNumber]) {
+//         res.send(mymovies[req.params.indexNumber])
+//     }
+//     else {
+//         res.send("Enter a valid index!!!!")
+//     }
+// })
+
+//*-------------------------------------------ques4-------------------------------------------------------*//
+
+// router.get('/films', function (req, res) {
+//     let movies = [
+//         { id: 1, name: "Chichore" },
+//         { id: 2, name: "Genius" },
+//         { id: 3, name: "Ms.Dhoni" },
+//         { id: 4, name: "3 Idiots" }
+//     ]
+//     res.send(movies)
+
+// })
+
+//*----------------------------------------------ques 5----------------------------------------------------*//
+
+router.get('/films/:filmsId', function (req, res) {
+    let movies = [
+        { id: 1, name: "Chichore" },
+        { id: 2, name: "Genius" },
+        { id: 3, name: "Ms.Dhoni" },
+        { id: 4, name: "3 Idiots" }
+    ]
+
+    for (let i = 0; i < movies.length; i++) {
+        if (movies[i].id == req.params.filmsId) {
+            return res.send(movies[i])
+        }
+
+    }
+   res.send("No movie present!")
+
+})
+
+
