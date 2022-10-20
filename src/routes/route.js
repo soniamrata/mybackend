d5e00e5551c832d66378ac5dfbf682b025c76486
amrata <soniamrata55@gmail.com>
2022-10-20 23:59:54 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,89 +1,89 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+// router.get('/students/:name', function(req, res) {
+//     let studentName = req.params.name
+//     console.log(studentName)
+//     res.send(studentName)
+// })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
-
-
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+// router.get("/random" , function(req, res) {
+//     res.send("hi there")
+// })
 
 
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
+// router.get("/test-api" , function(req, res) {
+//     res.send("hi FunctionUp")
+// })
 
 
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
+// router.get("/test-api-2" , function(req, res) {
+//     res.send("hi FunctionUp. This is another cool API")
+// })
 
 
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
+// router.get("/test-api-3" , function(req, res) {
+//     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
+// })
 
 
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
+// router.get("/test-api-4" , function(req, res) {
+//     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+// })
 
 
 
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
+// router.get("/test-api-5" , function(req, res) {
+//     res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+// })
+
+// router.get("/test-api-6" , function(req, res) {
+//     res.send({a:56, b: 45})
+// })
+
+// router.post("/test-post", function(req, res) {
+//     res.send([ 23, 45 , 6])
+// })
+
+
+// router.post("/test-post-2", function(req, res) {
+//     res.send(  { msg: "hi" , status: true }  )
+// })
+
+// router.post("/test-post-3", function(req, res) {
+//     // let id = req.body.user
+//     // let pwd= req.body.password
+
+//     // console.log( id , pwd)
+
+//     console.log( req.body )
+
+//     res.send(  { msg: "hi" , status: true }  )
+// })
+
+
+
+// router.post("/test-post-4", function(req, res) {
+//     let arr= [ 12, "functionup"]
+//     let ele= req.body.element
+//     arr.push(ele)
+//     res.send(  { msg: arr , status: true }  )
+// })
 
 
 
 
-// QUERY vs PARAMS
-// Params ( path params ):-  used to load different items/pages based on a variable value in url
-// params variable name  is not visible in  the url
+// // QUERY vs PARAMS
+// // Params ( path params ):-  used to load different items/pages based on a variable value in url
+// // params variable name  is not visible in  the url
 
 
-router.get("/wiki/:countryName", function(req, res) {
+// router.get("/wiki/:countryName", function(req, res) {
     
-    // ...
-    // hey nodejs , go and get me the data for countryName
-    res.send( {data: "something data"} )
-})
+//     // ...
+//     // hey nodejs , go and get me the data for countryName
+//     res.send( {data: "something data"} )
+// })
 
 
 
@@ -157,6 +157,42 @@ router.post( "/post-query-2", function (req, res){
         if ( myArr[i] > input )     finalArr.push( myArr[i])
     }
     res.send( {data: finalArr , status: true})
+})
+
+// router.post('/voting', function(req,res){
+//     let query=req.body.persons
+//     let body = req.query.votingAge
+//     let vote = []
+//     for(i = 0; i<query.length;i++ ){
+//         if(query[i].age>body){
+//             query[i].votingStatus = true;
+//             vote.push(query[i])
+
+//         }
+//     }
+//     console.log (vote)
+//     res.send(vote)
+// })
+router.post('/voting',function(req,res){
+
+    let arr =[]
+    let query = req.body.persons
+   let body=req.query.votingAge
+ 
+ for(let i=0;i<query.length;i++)
+ {
+    // persons[i].age>18 ? arr.push(persons[i]) : "no result"
+   
+    if(query[i].age>body)
+    {
+        query[i].votingStatus=true
+        arr.push(query[i])
+    }
+    
+  
+ } 
+ console.log(arr)
+    res.send(arr)
 })
 
 
