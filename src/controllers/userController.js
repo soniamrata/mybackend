c30729c78cf8b8f6fa185af2b840dc567dc5daaf
amrata <soniamrata55@gmail.com>
2022-11-07 00:06:05 +0530

    autherisation done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -41,23 +41,7 @@ const loginUser = async function (req, res) {
 };
 
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-
-  //If no token is present in the request header return error
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-
-  console.log(token);
   
-  // If a token is present then decode the token with verify function
-  // verify takes two inputs:
-  // Input 1 is the token to be decoded
-  // Input 2 is the same secret with which the token was generated
-  // Check the value of the decoded token yourself
-  let decodedToken = jwt.verify(token, "functionup-thorium");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
-
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
   if (!userDetails)
@@ -89,20 +73,6 @@ const postMessage = async function (req, res) {
     // Check if the token is present
     // Check if the token present is a valid token
     // Return a different error message in both these cases
-    let token = req.headers["x-auth-token"]
-    if(!token) return res.send({status: false, msg: "token must be present in the request header"})
-    let decodedToken = jwt.verify(token, 'functionup-thorium')
-
-    if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
-    
-    //userId for which the request is made. In this case message to be posted.
-    let userToBeModified = req.params.userId
-    //userId for the logged-in user
-    let userLoggedIn = decodedToken.userId
-
-    //userId comparision to check if the logged-in user is requesting for their own data
-    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
-
     let user = await userModel.findById(req.params.userId)
     if(!user) return res.send({status: false, msg: 'No such user exists'})
     
@@ -115,8 +85,18 @@ const postMessage = async function (req, res) {
     return res.send({status: true, data: updatedUser})
 }
 
+let deleteData = async function (req,res){
+  let userId = req.params.userId
+  let abc = await userModel.findOneAndUpdate({_id: userId, isDeleted: false},{$set:{isDeleted:true}})
+  
+  if(!abc){return res.send({status: false, msg:"User doesn't exist! OR already deleted."})}
+  
+  return res.send({status:true, msg:'Deleted Successfully'}) 
+  }
+
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
-module.exports.postMessage = postMessage
+module.exports.postMessage = postMessage;
+module.exports.deleteData=deleteData
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,56 @@
-const authenticate = function(req, req, next) {
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
+ 
+ 
+ const authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
-
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+  
+    //If no token is present in the request header return error
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+  
+    console.log(token);
+    
+    // If a token is present then decode the token with verify function
+    // verify takes two inputs:
+    // Input 1 is the token to be decoded
+    // Input 2 is the same secret with which the token was generated
+    // Check the value of the decoded token yourself
+    let decodedToken = jwt.verify(token, "functionup-thorium");
     next()
+    if (!decodedToken)
+      return res.send({ status: false, msg: "token is invalid" });
+  
+    
+  
 }
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
+
+const authrised = function(req, res, next) {
+
+  // comapre the logged in user's id and the id in request
+  let token = req.headers["x-auth-token"]
+  if(!token) return res.send({status: false, msg: "token must be present in the request header"})
+
+  let decodedToken = jwt.verify(token, 'functionup-thorium')
+
+  if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
+  
+  //userId for which the request is made. In this case message to be posted.
+  let userToBeModified = req.params.userId
+  //userId for the logged-in user
+  let userLoggedIn = decodedToken.userId
+  next()
+
+  //userId comparision to check if the logged-in user is requesting for their own data
+  if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+
+ 
+  
 }
+
+module.exports.authenticate=authenticate
+module.exports.authrised=authrised
